0fe90c699f5b65fdb36194a30ded05e1a7c59a47
Liam Cryan <limecrayon@gmail.com>
2022-03-17 11:28:54 -0700

    User doesn't need tests package installed

--- setup.py
+++ setup.py
@@ -9,14 +9,13 @@ install_requires = ['pytz', 'requests']
 with open('README.rst', 'r', encoding='utf-8') as f:
     readme = f.read()
 
-
 setup(name='stationtz',
       version='0.0.1',
       description='convert local time to utc time by airport station or vise-versa.',
       long_description=readme,
       author='Liam Cryan',
       author_email='cryan.liam@gmail.com',
-      packages=find_packages(),
+      packages=["stationtz"],
       install_requires=install_requires,
       include_package_data=True,
       url='https://github.com/liamcryan/airporttime',
